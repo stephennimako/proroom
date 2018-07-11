a49988dcf051c786bd0c1791b3196e5c35d7e998
Stephen <stephen.nimako@hotmail.co.uk>
2018-07-11 23:09:13 +0100

    Added logo

--- assets/javascripts/theme-scripts/agency.js
+++ assets/javascripts/theme-scripts/agency.js
@@ -4,7 +4,6 @@
     $(document).ready(function () {
         // Smooth scrolling using jQuery easing
         $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-            console.log('smooth scrolling');
             if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -19,7 +18,6 @@
 
         // Closes responsive menu when a scroll trigger link is clicked
         $('.js-scroll-trigger').click(function () {
-            console.log('close responsive menu');
             $('.navbar-collapse').collapse('hide');
         });
 
@@ -31,20 +29,6 @@
             });
         });
     });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-      var mainNav = $("#mainNav");
-      if (mainNav.offset() && mainNav.offset().top > 100) {
-        mainNav.addClass("navbar-shrink");
-    } else {
-      mainNav.removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
 
   // Hide navbar when modals trigger
   $('.portfolio-modal').on('show.bs.modal', function(e) {
