afdfbc1d0686803bdbfed07ca9fc46109ab9856b
Stephen <stephen.nimako@hotmail.co.uk>
2018-07-29 18:15:25 +0100

    Added footer with terms and conditions and privacy policy

--- assets/javascripts/theme-scripts/agency.js
+++ assets/javascripts/theme-scripts/agency.js
@@ -31,10 +31,10 @@
     });
 
   // Hide navbar when modals trigger
-  $('.portfolio-modal').on('show.bs.modal', function(e) {
+  $('.legal-modal').on('show.bs.modal', function(e) {
     $(".navbar").addClass("d-none");
   })
-  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+  $('.legal-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
   })
 
